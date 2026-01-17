86102a82bed40fe2f8e2f0936b55338fd0b9927c
Akong Rodney <rodneyakong@gmail.com>
2026-01-17 20:56:56 +0100

    Refactor an existing React application that uses prop drilling

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,18 @@
+import ProfilePage from './ProfilePage';
+import UserContext from './context/UserContext';
+import { jsxDEV } from "react/jsx-dev-runtime";
+
+function App() {
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com"
+  };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
