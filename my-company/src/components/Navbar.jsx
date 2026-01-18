24b5e4669211d087d2683b3d7fff9d1ab6cf6cc1
Akong Rodney <rodneyakong@gmail.com>
2026-01-19 00:27:24 +0100

    company website built

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{
+      backgroundColor: '#222',
+      padding: '15px'
+    }}>
+      <Link to="/" style={{ color: 'white', marginRight: '15px' }}>Home</Link>
+      <Link to="/about" style={{ color: 'white', marginRight: '15px' }}>About</Link>
+      <Link to="/services" style={{ color: 'white', marginRight: '15px' }}>Services</Link>
+      <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
