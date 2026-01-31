4c75fe954f13d862c237a2c7106377b2dbf8ecdf
Akong Rodney <rodneyakong@gmail.com>
2026-02-01 00:47:57 +0100

    Setting Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search App</h1>
+      <p>Project setup complete. Ready to build!</p>
+    </div>
+  );
+}
+
+export default App;
