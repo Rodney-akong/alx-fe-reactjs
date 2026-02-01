995b74ef296c92d81d3485455010eac7873a5838
Akong Rodney <rodneyakong@gmail.com>
2026-02-01 21:00:33 +0100

    Setting Up a React Project for GitHub User Search Application

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>GitHub User Search App</h1>
-      <p>Setup complete. Ready to build search feature.</p>
+      <Search />
     </div>
   );
 }
---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div>
+      <input 
+        type="text" 
+        placeholder="Search GitHub username..." 
+      />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default Search;
