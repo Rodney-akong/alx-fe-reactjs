d96d24dea8a270bbc4378216ab2e5b15abdbf5e1
Akong Rodney <rodneyakong@gmail.com>
2026-02-05 14:52:53 +0100

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -12,7 +12,7 @@ function UserProfile() {
         src="https://via.placeholder.com/150" 
         alt="User" 
         className="
-          w-24 h-24 md:w-36 md:h-36 
+          sm:w-24 sm:h-24 md:w-36 md:h-36 
           rounded-full mx-auto
         "
       />
