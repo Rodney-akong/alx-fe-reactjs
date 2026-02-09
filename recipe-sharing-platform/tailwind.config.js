19eaf72dbc8280fd295b8f737fb3d3eb3d98d71b
Akong Rodney <rodneyakong@gmail.com>
2026-02-09 23:08:05 +0100

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",          // main HTML in Vite root
+    "./public/index.html",          // main HTML in Vite root
     "./src/**/*.{js,jsx,ts,tsx}"  // all React components
   ],
   theme: {
